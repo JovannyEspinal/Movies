e9af1bce98a27c1e81fe487f457c53c0aebc605a
JovannyEspinal <espinaljovanny@icloud.com>
2016-05-27 23:16:40 -0400

    Installed ReactiveCocoa framework

--- Movies/Movies/ViewController.swift
+++ Movies/Movies/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
